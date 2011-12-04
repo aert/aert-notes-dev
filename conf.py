248d5567856648ae802bb3c81d13d92f2daa895c
aert <spam.aert@gmail.com>
2011-12-04 13:15:38 +0100

    new file:   content/git-contributing.rst ... modified:   README.rst modified:   conf.py modified:   content/git.rst

--- conf.py
+++ conf.py
@@ -13,6 +13,8 @@
 
 import sys, os
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -91,7 +93,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+if on_rtd:
+    html_theme = 'nature'
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- content/git.rst
+++ content/git.rst
@@ -15,6 +15,10 @@ My Aliases
         lg = log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
         co = commit -a
 
+Contributing
+============
+
+See :ref:`Contributing <git-contributing>`
 
 Get Total Commits Number in git
 ===============================
